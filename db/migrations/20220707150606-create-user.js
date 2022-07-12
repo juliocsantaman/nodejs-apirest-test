7847a86d14f2ccfbb1129e23562a983077226ab9
juliocsantaman <juliocsantaman@gmail.com>
2022-07-12 11:53:55 -0500

    fix migrations.

--- db/migrations/20220707150606-create-user.js
+++ db/migrations/20220707150606-create-user.js
@@ -1,25 +1,36 @@
 'use strict';
 
-const { UserSchema, USER_TABLE } = require('../models/user.model');
+const { USER_TABLE } = require('../models/user.model');
+const { DataTypes, Sequelize } = require('sequelize');
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    await queryInterface.createTable(USER_TABLE, UserSchema);
+  async up (queryInterface) {
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'create_at',
+        defaultValue: Sequelize.NOW
+      }
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.drop(USER_TABLE);
+  async down (queryInterface) {
+    await queryInterface.dropTable(USER_TABLE);
   }
 };
--- db/migrations/20220707162722-task-table.js
+++ db/migrations/20220707162722-task-table.js
@@ -8,6 +8,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.drop(TASK_TABLE);
+    await queryInterface.dropTable(TASK_TABLE);
   }
 };
